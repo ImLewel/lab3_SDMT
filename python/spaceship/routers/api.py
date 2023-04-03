1d74bc547776a76d7eb4a1ae09023a00f172a0ca
Lev Pavelko <neroheroes6@gmail.com>
2023-04-03 07:32:09 +0300

    Returned normal Dockerfile, added numpy and matrix multiplier

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +7,13 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrixmult')
+def matrixmult() -> dict:
+    first = np.random.rand(10, 10)
+    second = np.random.rand(10, 10)
+    return {
+        "first": first.tolist(),
+        "second": second.tolist(),
+        "result": np.matmul(first, second).tolist()
+    }
