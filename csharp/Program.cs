3089be33d8eeb8a721c160e07b3ec37eb70e19a9
Lev Pavelko <neroheroes6@gmail.com>
2023-04-03 13:49:46 +0300

    Added third task (csharp)
    Also added Dockerfile for it

---
+++ csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program {
+  static void Main(string[] args) {
+    Console.WriteLine("If you see this text, program run successfully!");
+    string name = Ask();
+    Console.WriteLine($"Hello, {name}!");
+    string Ask() {
+      Console.WriteLine("So, what's your name?");
+      string res = Console.ReadLine() ?? "null (good name tho)";
+      return (res == "" || res == " ") ? Ask() : res;
+    }
+  }
+}
